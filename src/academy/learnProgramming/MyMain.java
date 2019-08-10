06cabfd94a3e5f655fc12ddf8bcbb7699c47bc44
sarita <sarita.chetak@gmail.com>
2019-08-10 17:40:55 +0100

    dfghjkl;

---
+++ src/academy/learnProgramming/MyMain.java
@@ -0,0 +1,11 @@
+package academy.learnProgramming;
+
+public class MyMain {
+    public static void main(String[] args) {
+        Person objectPerson1= new Person();
+        Person objectperson2=new Person();
+        Person  objectPerson3=new Person();
+
+        objectPerson1.
+    }
+}
---
+++ src/academy/learnProgramming/Person.java
@@ -0,0 +1,38 @@
+package academy.learnProgramming;
+
+public class Person {
+    String firstName;
+    private String lastName;
+    private int age;
+
+
+    public void setFirstName(String firstName) {
+
+        this.firstName = firstName;
+
+    }
+
+    public void setLastName(String lastName) {
+
+        this.lastName = lastName;
+
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+}
+}
